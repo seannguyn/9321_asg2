f7d24210f047d950dce609ffe1d39d072c1c34a5
my mac <turlyjohn@foxmail.com>
2018-09-30 02:15:19 +1000

    try to build up linear regression model

---
+++ MovieTraining.py
@@ -0,0 +1,79 @@
+class MovieDataTrainingMatrix:
+    gross=[]
+    budget=[]
+    variables=[]
+    imdbScore=[]
+
+
+    @classmethod
+    def add_value_adding_ability(cls,df):
+        directorDict = dict()
+        for index, row in df.iterrows():
+
+            directorName = row["director_name"]
+            revenue=row["gross"]
+            budget=row["budget"]
+            if directorName not in directorDict.keys():
+                directorDict[directorName]={"num": 1,
+                                            "ability": revenue/budget
+                                            }
+            else:
+                directorDict[directorName]["ability"] = (directorDict[directorName]["ability"] *
+                           directorDict[directorName]["num"]+revenue/budget)/(directorDict[directorName]["num"]+1)
+                directorDict[directorName]["ability"]+=1
+        return directorDict
+
+    @classmethod
+    def loadDf(cls,df):
+
+        variables=[]
+        gross=[]
+        budget=[]
+        imdbScore = []
+        directorValueAddingAbility = cls.add_value_adding_ability(df)
+        for index, row in df.iterrows():
+            if abs(row["budget"]-row["gross"])/row["budget"]>4:
+                continue
+            #d1 = row["director_facebook_likes"]
+            a1 = row["actor_1_facebook_likes"]
+            a2 = row["actor_2_facebook_likes"]
+            a3 = row["actor_3_facebook_likes"]
+            variables.append([(a1+a2+a3),row["budget"]/row["duration"],row["movie_facebook_likes"],
+                              directorValueAddingAbility[row["director_name"]]["ability"] * row["budget"]])
+            gross.append([row["gross"]])
+            budget.append([row["budget"]])
+            imdbScore.append(row["imdb_score"])
+        return MovieDataTrainingMatrix(gross,budget,variables,imdbScore)
+
+    """
+        df is a dataframe
+    """
+    def __init__(self,gross,budget,variables,imdbScore):
+        self.gross=gross
+        self.budget=budget
+        self.variables=variables
+        self.imdbScore=imdbScore
+
+
+    def cutOut(self,percentage):
+        percentageThisMatrixKeep=(1 - percentage)
+        theLargestIndexAfterCut = int(percentageThisMatrixKeep * len(self.gross))
+        newMovieDataTrainingMatrix = MovieDataTrainingMatrix(
+            self.gross[theLargestIndexAfterCut:],
+            self.budget[theLargestIndexAfterCut:],
+            self.variables[theLargestIndexAfterCut:],
+            self.imdbScore[theLargestIndexAfterCut:],
+        )
+        self.gross = self.gross[:theLargestIndexAfterCut]
+        self.budget = self.budget[:theLargestIndexAfterCut]
+        self.variables = self.variables[:theLargestIndexAfterCut]
+        self.imdbScore = self.imdbScore[:theLargestIndexAfterCut]
+        return newMovieDataTrainingMatrix
+
+
+    def show(self):
+        print(self.gross)
+        print()
+        print(self.budget)
+        print()
+        print(self.variables)
--- ass_3.py
+++ ass_3.py
@@ -1,7 +1,93 @@
+import pandas as pd
+from  MovieTraining import MovieDataTrainingMatrix
+import np
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score, accuracy_score, recall_score
+import matplotlib.pyplot as mp
+
 
 def hello():
     print("hello world!!")
     print("start assignment")
-	print("join repo")
+    print("join repo")
+
+def readData():
+    return pd.read_csv("movie_metadata.csv")
+
+def dataCleaning(df):
+    df.drop(columns=["language",
+                     "director_facebook_likes",
+                     #"country",
+                     #"content_rating",
+                     "movie_imdb_link",
+                     "facenumber_in_poster",
+                     "plot_keywords",
+                     "num_voted_users",
+                     "movie_title",
+                     "num_critic_for_reviews",
+                     "color",
+                     #"movie_facebook_likes",
+                     "aspect_ratio",
+                     "title_year",
+                     "num_user_for_reviews",
+                     "cast_total_facebook_likes"],
+            inplace=True)
+    df = df.replace(0, np.nan)
+    df = df[df.gross.notnull()]
+    df = df[df.budget.notnull()]
+    df = df[df.gross.notnull()]
+    df = df[df.country == "USA"]
+
+    df.dropna(inplace=True)
+    return df
+
+
+
+def predict(df):
+    mm = MovieDataTrainingMatrix.loadDf(df)
+    mm2 = mm.cutOut(0.25)
+    model = LinearRegression()
+    model.fit(mm.variables,mm.gross)
+
+    predictions = model.predict(mm2.variables)
+    counter=0
+    for i,prediction in enumerate(predictions):
+        print("Predicted: {}, Target: {}".format(prediction,mm2.gross[i]))
+        diff = abs(prediction - mm2.gross[i])/ mm2.gross[i]
+        if diff < 0.5 :
+            counter+=1
+        print("Difference percentage {}%".format(diff*100))
+    print("R-squard: {}".format(model.score(mm2.variables, mm2.gross)))
+
+    print("{} samples has difference less than 50% over {}".format(counter,len(predictions)))
+
+def predictKnn(df):
+    mm = MovieDataTrainingMatrix.loadDf(df)
+    mm2 = mm.cutOut(0.25)
+    knn = KNeighborsClassifier()
+    knn.fit(mm.variables, mm.gross)
+    predictions = knn.predict(mm2.variables)
+    print("confusion_matrix:\n", confusion_matrix(mm2.gross, predictions))
+    print("precision:\t", precision_score(mm2.gross, predictions, average=None))
+    print("recall:\t\t", recall_score(mm2.gross, predictions, average=None))
+    print("accuracy:\t", accuracy_score(mm2.gross, predictions))
+
+
 if __name__ == '__main__':
     hello()
+    df= readData()
+    df =dataCleaning(df)
+    #predict(df)
+    #predictKnn(df)
+    df.to_csv("movie_refined.csv")
+    df["all_Like"] = df.actor_1_facebook_likes+df.actor_3_facebook_likes+df.actor_2_facebook_likes
+    df["buget_duration"]=df.budget/df.duration
+    df.plot.scatter(y="gross",x="budget", title = "buget v gross")
+    df.plot.scatter(y="gross", x="all_Like", title = "popularity of actors v gross ")
+    df.plot.scatter(y="gross", x="buget_duration",title = "buget_duration v gross")
+    df.plot.scatter(y="gross", x="duration" , title = "duration v gross")
+    df.plot.scatter(y="gross", x="imdb_score", title="imdb-score v gross")
+
+    mp.show()
